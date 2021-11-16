daae804f56c1f35ae42b7bf34e80a5f8e98c9920
VaishaliNandhagopal <vaishalinandhagopal@gmail.com>
2021-11-17 01:23:20 +0530

    [Application] Hacker News to handle the reresh listener to load the content at bottom of list

--- app/src/main/java/com/android/news/search/Search.java
+++
@@ -1,4 +0,0 @@
-package com.android.news.search;
-//https://www.googleapis.com/customsearch/v1?q=love&key=AIzaSyCGTHMilAaxeKEn12Xzq99zYGjY7YYAurE&cx=d6359fc248fc6acdb&alt=json
-public class Search {
-}
--- app/src/main/java/com/android/news/search/SearchActivity.java
+++ app/src/main/java/com/android/news/search/SearchActivity.java
@@ -1,18 +1,23 @@
 package com.android.news;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.news.adapter.SearchAdapter;
 import com.android.news.repo.SearchApi;
+import com.android.news.searchmodel.ItemsItem;
 import com.android.news.searchmodel.Response;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,38 +29,50 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends Activity {
+    private final String QUERY_PARAM_API_KEY = "AIzaSyCGTHMilAaxeKEn12Xzq99zYGjY7YYAurE";
+    private final String QUERY_PARAM_CX = "d6359fc248fc6acdb";
+    private final String BASE_URL = "https://www.googleapis.com/";
     EditText oseachquery;
     ImageButton oSearch;
+    SearchActivity mContext;
     RecyclerView msearhResult;
     SearchApi mSearchApi;
     Map<String, String> query = new HashMap<>();
     Call<Response> mSearchResponse;
-
     View mView;
-    private final String QUERY_PARAM_API_KEY = "AIzaSyCGTHMilAaxeKEn12Xzq99zYGjY7YYAurE";
-    private final String QUERY_PARAM_CX = "d6359fc248fc6acdb";
-    private final String BASE_URL = "https://www.googleapis.com/";
+    private List<ItemsItem> getItems;
+    private SearchAdapter mSearchAdapter;
 
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        getItems = new ArrayList<>();
+        mSearchAdapter = new SearchAdapter(this, getItems);
+        mContext = this;
         oseachquery = findViewById(R.id.searchquery);
         oSearch = findViewById(R.id.goearch);
         oseachquery.setHintTextColor(R.color.black);
         mView = findViewById(R.id.searchresult);
         msearhResult = mView.findViewById(R.id.recyclerView);
+        msearhResult.setLayoutManager(new LinearLayoutManager(mContext));
         oSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makerequest();
+                if(oseachquery.getText().toString()!=null && !oseachquery.getText().toString().isEmpty())
+                {
+                    makerequest(oseachquery.getText().toString());
+                }else{
+                    Toast.makeText(mContext,"Enter valid search",Toast.LENGTH_LONG).show();
+                }
             }
         });
+
     }
 
-    public void makerequest() {
+    public void makerequest(String mquery) {
         Retrofit retrofit = null;
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -70,7 +87,7 @@ public class SearchActivity extends AppCompatActivity {
                     .build();
         }
         mSearchApi = retrofit.create(SearchApi.class);
-        query.put("q", "love");
+        query.put("q", mquery);
         query.put("key", QUERY_PARAM_API_KEY);
         query.put("cx", QUERY_PARAM_CX);
         query.put("num", "10");
@@ -82,14 +99,23 @@ public class SearchActivity extends AppCompatActivity {
             mSearchResponse.enqueue(new retrofit2.Callback<Response>() {
                 @Override
                 public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
-
+                    if (response.isSuccessful()) {
+                        Response mResponse = response.body();
+                        assert mResponse != null;
+                        Log.d("search", "success " + mResponse.getItems());
+                        getItems.addAll(mResponse.getItems());
+                    }
                 }
 
                 @Override
                 public void onFailure(Call<Response> call, Throwable t) {
                     Log.d("main", "onFailure: " + t.getMessage());
+                    Toast.makeText(mContext,"No internet connection",Toast.LENGTH_LONG).show();
                 }
             });
         }
+        mSearchAdapter.getAllItems(getItems);
+        msearhResult.setAdapter(mSearchAdapter);
+
     }
 }
